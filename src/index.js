bbf7feabb21c3b61d0a412cdac49effaeade65cb
Sibevin Wang <sibevin@gmail.com>
2017-03-30 18:18:43 +0800

    Implement word count feature
    1. Update pacakge and readme
    2. Implement plugin
    3. Add an icon

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 'use strict';
 
+const ICON_FILE = require('./icon.png');
+const WC_REGEX = /wc\s+(.*)/i;
+
 const plugin = ({term, display, actions}) => {
-  // It is your main plugin function
-  // do something and call display() with your results
+  if (WC_REGEX.test(term)) {
+    let [, query] = WC_REGEX.exec(term);
+    let count = query.length;
+    display({
+      icon:       ICON_FILE,
+      title:      `${count}`,
+      getPreview: () => `<div style="font-size: 2em;">${count} word(s)<br/>${query}</div>`
+    });
+  }
 };
 
 module.exports = {
-  fn: plugin
+  fn: plugin,
+  icon: ICON_FILE,
+  name: 'Word Count',
+  keyword: 'wc'
 }
